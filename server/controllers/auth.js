054e23c62a2e4a455ab2dfc146f5cfc5b4b0ceff
Adam Demaree <demareeadam1@gmail.com>
2017-07-11 09:45:08 -0400

    user function added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,30 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res){
 	//res.send("User Auth!");
 
 	console.log(req.body)
 	var email = req.body.email
 	var password = req.body.password
+
+	User.findOne({email: email}, function(err, existingUser){
+		if(err){
+			return next(err);
+		}
+		if(existingUser){
+			return res.status(418).send('Emails already in use');
+		}
+
+		let user = new User({
+			email: email,
+			password: password
+		});
+
+		user.save(function(err){
+			if(err){
+				return next(err)
+			}
+			res.json({success:true})
+		})
+	})
 }

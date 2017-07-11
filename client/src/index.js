54c27bf03338699599316f4e7ab238c4eae173cd
Adam Demaree <demareeadam1@gmail.com>
2017-07-11 19:40:17 -0400

    navbar added

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,15 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 var createStoreWithMiddelware = applyMiddleware()(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddelware(reducers)}>
-		<App />
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+			</Route>
+		</Router>
 	</Provider>
 	, document.querySelector('.container'));

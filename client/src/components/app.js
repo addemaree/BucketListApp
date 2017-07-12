7ad2648ab148b118d04628a40fc09a6d7813d86b
Adam Demaree <demareeadam1@gmail.com>
2017-07-12 18:26:51 -0400

    video API key added

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 import NavBarHeader from './nav';
+import Video from './video/video.js';
 
 export default class App extends Component{
 	render(){
 		return(
 				<div>
 					<NavBarHeader />
+					<Video />
 				</div>
 			);
 	}

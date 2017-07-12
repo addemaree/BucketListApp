72454d16aa431754515377696fa4f79ac70b8c76
Adam Demaree <demareeadam1@gmail.com>
2017-07-12 19:56:48 -0400

    IM STUCK ON MOD 12

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 import NavBarHeader from './nav';
-import Video from './video/video.js';
+//import Video from './video/video.js';
+import BandList from '../containers/BandList';
 
 export default class App extends Component{
 	render(){
 		return(
 				<div>
 					<NavBarHeader />
-					<Video />
+					//<Video />
+					<BandList />
 				</div>
 			);
 	}
